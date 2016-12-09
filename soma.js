a76577010ef866b53319154a9733572f5c7dff06
Paula Magalhães <pmagalhaesa@hotmail.com>
2016-12-08 23:31:09 -0200

    soma_js

---
+++ soma.js
@@ -0,0 +1,8 @@
+function soma(num1, num2)
+{
+alert("Resultado da soma: "+(num1+num2));
+}
+num1=parseInt(window.prompt("Digite o primeiro numero: ")); 
+num2=parseInt(window.prompt("Digite o segundo numero: ")); 
+soma(num1, num2);
+
